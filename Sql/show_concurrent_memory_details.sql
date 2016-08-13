2dc5970f3a45235c795b96be8f7b66614128fe30
Zvika Gutkin <zvika.gutkin@convertro.com>
2016-08-13 11:44:28 +0300

    fix concurrent memory

--- Sql/show_concurrent_memory_details.sql
+++ Sql/show_concurrent_memory_details.sql
@@ -16,18 +16,19 @@ from 	(
 left outer join
 		(select  rnum,trunc(queue_entry_timestamp,'SS') as  queue_entry_timestamp
 		,trunc(acquisition_timestamp,'SS')  as acquisition_timestamp
-		,transaction_id,statement_id,(memory_inuse_kb/1024)::int memory_inuse_mb
+		,trunc(release_timestamp,'SS')  as release_timestamp
+		,transaction_id,statement_id,(memory_inuse_kb/1024)::int memory_inuse_mb,pool_name
  		from (
  		select
  		max(extract(EPOCH FROM (acquisition_timestamp  - queue_entry_timestamp ))) over (partition by transaction_id,statement_id) as max_queue_time,memory_inuse_kb,
  		row_number ()  over (partition by transaction_id,statement_id order by memory_inuse_kb desc ) as rnum,
  		queue_entry_timestamp::timestamptz at timezone 'US/Pacific' as queue_entry_timestamp,acquisition_timestamp::timestamptz at timezone 'US/Pacific' as acquisition_timestamp,release_timestamp::timestamptz at timezone 'US/Pacific' as release_timestamp
- 		,transaction_id,statement_id
+ 		,transaction_id,statement_id,pool_name
  		from sys_history.hist_v_monitor_resource_acquisitions
-		  where queue_entry_timestamp::timestamptz at timezone 'US/Pacific' <=  :2  and acquisition_timestamp::timestamptz at timezone 'US/Pacific' >=  :1
+		  where queue_entry_timestamp::timestamptz at timezone 'US/Pacific' <=  :2  and release_timestamp::timestamptz at timezone 'US/Pacific' >=  :1
 -- 		and transaction_id = '81064793300185688' and statement_id = 113
  		) a
  		where rnum =1
 ) mem
-on b.slice_seconds between mem.queue_entry_timestamp and mem.acquisition_timestamp
+on b.slice_seconds between mem.queue_entry_timestamp and mem.release_timestamp
 order by slice_seconds

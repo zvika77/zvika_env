ad699a6d36d73ed6b99ff9747a273f5887c05776
zvika <zvika.gutkin@gmail.com>
2018-03-15 12:49:14 +0000

    more sql fixes

--- Redshift/Sql/show_queries.sql
+++ Redshift/Sql/show_queries.sql
@@ -1,4 +1,4 @@
-select trim(usename) user_name,xid,trim(label) as label,date_trunc('second',starttime) as start_time,datediff('minute', starttime,endtime) as dur_min,type,substring(text,1,100) as text
+select trim(usename) user_name,xid,trim(label) as label,date_trunc('second',starttime) as start_time,datediff('second', starttime,endtime) as dur_sec,type,substring(text,1,100) as text
 from SVL_STATEMENTTEXT svs join pg_user_info pui on (svs.userid = pui.usesysid)
 where trim(usename) ilike :1
 and sequence = 0
--- Redshift/Sql/show_query_summary.sql
+++ Redshift/Sql/show_query_summary.sql
@@ -1,6 +1,8 @@
 select query,stm,seg,step,maxtime/1000000||'/'||avgtime/1000000 as max_avg_time_sec,ROWs
 ,(bytes/1024/1024)::int as MB,rate_row||'/'||(rate_byte/1024/1024)::int as rate_row_mb
-,label,decode(is_diskbased,'t','<===',null) as disk,(workmem/1024/1024)::int mem_mb,rows_pre_filter
+,label,decode(is_diskbased,'t','<===',null) as disk,
+  case when workmem >0 then (workmem/ (select count(*) slices from stv_slices)/1024/1024)::int else 0 end mem_mb_per_slice
+  ,case when workmem >0 then (workmem/1024/1024)::int else 0 end mem_mb_all_slices
+  ,rows_pre_filter
 from svl_query_summary where query = :1
-order by query, seg, step;
-
+order by query, seg, step
